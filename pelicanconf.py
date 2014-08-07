8fd9ea2ef92983e473665f0281013b3896ab7cb5
Steve Clement <steve@localhost.lu>
2014-08-07 09:35:42 +0200

    - added initial Makefiles et al.

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'CodeClubLu'
+SITENAME = u'CoderDojoLu'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Luxembourg'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('CodeClubLu', 'http://codeclub.lu/'),)
+
+# Social widget
+SOCIAL = (('CoderDojo on TW', 'https://twitter.com/CoderDojoLu'),
+         #('CoderDojo on FB','https://www.facebook.com/CoderDojoLu'),
+         ('CoderDojo on FB','https://www.facebook.com/pages/CoderDojo-Luxembourg/1494650000781128'),
+         ('CodeClubLu on TW','https://twitter.com/CodeClubLux'),
+         ('CodeClubLu on FB','https://www.facebook.com/codeclub.lu'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
